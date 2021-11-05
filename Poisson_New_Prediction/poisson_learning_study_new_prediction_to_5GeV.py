c54063747479c69935b2d5df332bb30ba0e98179
alan200276 <alan200276@gmail.com>
2021-11-05 01:43:42 +0000

    update script: add model 3 and model 4

--- Poisson_New_Prediction/poisson_learning_study_new_prediction_to_5GeV.py
+++ Poisson_New_Prediction/poisson_learning_study_new_prediction_to_5GeV.py
@@ -77,39 +77,150 @@ except:
 Define Model
 """
 #======================================================#
-"""
-Model 1 #11/04 modified
-"""
-def my_loss_fn(y_true, y_pred):
-    squared_difference = tf.square(y_true - y_pred)
-    mse = tf.reduce_mean(squared_difference, axis=-1) 
+# """
+# Model 1 #11/04 modified
+# """
+# def my_loss_fn(y_true, y_pred):
+#     squared_difference = tf.square(y_true - y_pred)
+#     mse = tf.reduce_mean(squared_difference, axis=-1) 
     
-    preserve_length = tf.sqrt(tf.reduce_sum(tf.square(y_pred), axis=-1))
-    preserve_length = tf.abs(tf.subtract(preserve_length, 1))
+#     preserve_length = tf.sqrt(tf.reduce_sum(tf.square(y_pred), axis=-1))
+#     preserve_length = tf.abs(tf.subtract(preserve_length, 1))
     
-    loss = tf.add(mse, preserve_length)
-    return loss #tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
+#     loss = tf.add(mse, preserve_length)
+#     return loss #tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
 
+# def Regression_Model(trig=False):
+#     #Ref: https://towardsdatascience.com/can-machine-learn-the-concept-of-sine-4047dced3f11
+#     inputs = Input(shape=(36,4),name = 'input')
+#     conv1d_1 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_1')(inputs)
+#     conv1d_2 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_2')(conv1d_1)
+#     flatten_1 = Flatten(name = "flatten_1")(conv1d_2)
+#     dense_1 = Dense(512, activation='relu', name = 'dense_1')(flatten_1)
+#     dense_2 = Dense(256, activation='relu', name = 'dense_2')(dense_1)
+#     dense_3 = Dense(256, activation='relu', name = 'dense_3')(dense_2)
+#     dense_4 = Dense(128, activation='relu', name = 'dense_4')(dense_3)
+#     dense_5 = Dense(128, activation='relu', name = 'dense_5')(dense_4)
+#     if trig == False:
+#         out1 = Dense(1, activation='linear', name = "output1")(dense_5)
+# #         out2 = Dense(1, activation='linear', name = "output2")(dense_5)
+#     elif trig == True:
+#         out1 = Dense(4, activation='linear', name = "output1")(dense_5)
+# #         out2 = Dense(2, activation='linear', name = "output2")(dense_5)
+
+# #     regression_model = Model(inputs=inputs, outputs=[out1,out2], name = 'Model')
+#     regression_model = Model(inputs=inputs, outputs=out1, name = 'Model')
+
+
+
+# #     model_opt = keras.optimizers.Adadelta()
+#     model_opt = keras.optimizers.Adam()
+
+#     regression_model.compile(loss="mean_squared_error",
+#                        optimizer=model_opt,
+#                        metrics=["mse","mae"])
+
+#     return regression_model
+
+# """
+# Model 2 #11/04 modified
+# """
+# def Regression_Model(trig=False):
+#     #Ref: https://towardsdatascience.com/can-machine-learn-the-concept-of-sine-4047dced3f11
+#     inputs = Input(shape=(36,4),name = 'input')
+#     conv1d_1 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_1')(inputs)
+#     pooling1d_1 = MaxPooling1D(pool_size=2, strides=None, padding="valid", name = 'pooling1d_1')(conv1d_1)
+#     conv1d_2 = Conv1D(filters=5, kernel_size=5,strides=1, activation='relu', name = 'conv1d_2')(pooling1d_1)
+#     pooling1d_2 = MaxPooling1D(pool_size=2, strides=None, padding="valid", name = 'pooling1d_2')(conv1d_2)
+#     flatten_1 = Flatten(name = "flatten_1")(pooling1d_2)
+#     dense_1 = Dense(512, activation='relu', name = 'dense_1')(flatten_1)
+#     dense_2 = Dense(256, activation='relu', name = 'dense_2')(dense_1)
+#     dense_3 = Dense(256, activation='relu', name = 'dense_3')(dense_2)
+#     dense_4 = Dense(128, activation='relu', name = 'dense_4')(dense_3)
+#     dense_5 = Dense(128, activation='relu', name = 'dense_5')(dense_4)
+    
+#     if trig == False:
+#         out1 = Dense(1, activation='linear', name = "output1")(dense_5)
+# #         out2 = Dense(1, activation='linear', name = "output2")(dense_5)
+#     elif trig == True:
+#         out1 = Dense(4, activation='linear', name = "output1")(dense_5)
+# #         out2 = Dense(2, activation='linear', name = "output2")(dense_5)
+
+# #     regression_model = Model(inputs=inputs, outputs=[out1,out2], name = 'Model')
+#     regression_model = Model(inputs=inputs, outputs=out1, name = 'Model')
+
+
+
+# #     model_opt = keras.optimizers.Adadelta()
+#     model_opt = keras.optimizers.Adam()
+
+#     regression_model.compile(loss="mean_squared_error",
+#                        optimizer=model_opt,
+#                        metrics=["mse","mae"])
+
+#     return regression_model
+
+
+# """
+# Model 3 #11/05 modified
+# """
+# def Regression_Model(trig=False):
+#     #Ref: https://towardsdatascience.com/can-machine-learn-the-concept-of-sine-4047dced3f11
+#     inputs = Input(shape=(36,4),name = 'input')
+#     conv1d_1 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_1')(inputs)
+#     conv1d_2 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_2')(conv1d_1)
+#     flatten_1 = Flatten(name = "flatten_1")(conv1d_2)
+#     dense_1 = Dense(512, activation='relu', name = 'dense_1')(flatten_1)
+#     dense_2 = Dense(256, activation='relu', name = 'dense_2')(dense_1)
+#     dense_3 = Dense(256, activation='relu', name = 'dense_3')(dense_2)
+#     dense_4 = Dense(128, activation='relu', name = 'dense_4')(dense_3)
+#     dense_5 = Dense(128, activation='relu', name = 'dense_5')(dense_4)
+    
+#     if trig == False:
+#         out1 = Dense(1, activation='linear', name = "output1")(dense_5)
+        
+#     elif trig == True:
+#         out_delta = Dense(2, activation='linear', name = "out_delta")(dense_5)
+#         out_theta23 = Dense(1, activation='relu', name = "out_theta23")(dense_5)
+
+#     regression_model = Model(inputs=inputs, outputs=[out_delta, out_theta23], name = 'Model_3')
+
+
+
+# #     model_opt = keras.optimizers.Adadelta()
+#     model_opt = keras.optimizers.Adam()
+
+#     regression_model.compile(loss="mean_squared_error",
+#                        optimizer=model_opt,
+#                        metrics=["mse","mae"])
+
+#     return regression_model
+
+"""
+Model 4 #11/05 modified
+"""
 def Regression_Model(trig=False):
     #Ref: https://towardsdatascience.com/can-machine-learn-the-concept-of-sine-4047dced3f11
     inputs = Input(shape=(36,4),name = 'input')
     conv1d_1 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_1')(inputs)
-    conv1d_2 = Conv1D(filters=5, kernel_size=10,strides=1, activation='relu', name = 'conv1d_2')(conv1d_1)
-    flatten_1 = Flatten(name = "flatten_1")(conv1d_2)
+    pooling1d_1 = MaxPooling1D(pool_size=2, strides=None, padding="valid", name = 'pooling1d_1')(conv1d_1)
+    conv1d_2 = Conv1D(filters=5, kernel_size=5,strides=1, activation='relu', name = 'conv1d_2')(pooling1d_1)
+    pooling1d_2 = MaxPooling1D(pool_size=2, strides=None, padding="valid", name = 'pooling1d_2')(conv1d_2)
+    flatten_1 = Flatten(name = "flatten_1")(pooling1d_2)
     dense_1 = Dense(512, activation='relu', name = 'dense_1')(flatten_1)
     dense_2 = Dense(256, activation='relu', name = 'dense_2')(dense_1)
     dense_3 = Dense(256, activation='relu', name = 'dense_3')(dense_2)
     dense_4 = Dense(128, activation='relu', name = 'dense_4')(dense_3)
     dense_5 = Dense(128, activation='relu', name = 'dense_5')(dense_4)
+    
     if trig == False:
         out1 = Dense(1, activation='linear', name = "output1")(dense_5)
-#         out2 = Dense(1, activation='linear', name = "output2")(dense_5)
+        
     elif trig == True:
-        out1 = Dense(4, activation='linear', name = "output1")(dense_5)
-#         out2 = Dense(2, activation='linear', name = "output2")(dense_5)
+        out_delta = Dense(2, activation='linear', name = "out_delta")(dense_5)
+        out_theta23 = Dense(1, activation='relu', name = "out_theta23")(dense_5)
 
-#     regression_model = Model(inputs=inputs, outputs=[out1,out2], name = 'Model')
-    regression_model = Model(inputs=inputs, outputs=out1, name = 'Model')
+    regression_model = Model(inputs=inputs, outputs=[out_delta, out_theta23], name = 'Model_3')
 
 
 
@@ -145,11 +256,15 @@ target = target/180*np.pi
 
 x_train = data_all[:900000]
 y_train = target[:900000]
-y_train = np.column_stack([np.sin(y_train[:,0]), np.cos(y_train[:,0]), np.sin(y_train[:,1]), np.cos(y_train[:,1])]) # 10/26 modified
+# y_train = np.column_stack([np.sin(y_train[:,0]), np.cos(y_train[:,0]), np.sin(y_train[:,1]), np.cos(y_train[:,1])]) # 11/04 modified
+y_train_delta = np.column_stack([np.sin(y_train[:,0]), np.cos(y_train[:,0])]) # 11/05 modified
+y_train_theta23 = y_train[:,1]
 
 x_test = data_all[900000:]
 y_test = target[900000:]
-y_test = np.column_stack([np.sin(y_test[:,0]), np.cos(y_test[:,0]), np.sin(y_test[:,1]), np.cos(y_test[:,1])]) # 10/26 modified
+# y_test = np.column_stack([np.sin(y_test[:,0]), np.cos(y_test[:,0]), np.sin(y_test[:,1]), np.cos(y_test[:,1])]) # 11/04  modified
+y_test_delta = np.column_stack([np.sin(y_test[:,0]), np.cos(y_test[:,0])]) # 11/05  modified
+y_test_theta23 = y_test[:,1]
 
 logging.info("X train shape: {}".format(x_train.shape))
 logging.info("X test shape: {}".format(x_test.shape))
@@ -209,14 +324,15 @@ for i in tqdm(range(0,300,1)):
     """
     check_list=[]
     csv_logger = CSVLogger("./Training_loss/" + str(experiment) + "_" + 
-                           "training_log_poisson_" +str(i)+ "_1.csv")
+                           "training_log_poisson_" +str(i)+ "_4.csv")
 
 
     check_list.append(csv_logger)
 
 
     model.fit( x_train_poisson,
-               y_train,
+#                y_train, # 11/05 modified
+              [y_train_delta, y_train_theta23],  # 11/05 modified
                validation_split = 0.1,
                batch_size=64,
                epochs=1,
@@ -226,7 +342,7 @@ for i in tqdm(range(0,300,1)):
              )
 
     model.save("./Model/" + str(experiment) + "_" + 
-                           "poisson_" + str(i)+ "_1.h5")
+                           "poisson_" + str(i)+ "_4.h5")
 #======================================================#
     
 
